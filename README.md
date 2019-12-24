ba9d3b8e965d335f75cd5d944e748fe3d0d58e52
yangheng <1220626014@qq.com>
2019-12-24 15:39:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flutter  省市区三级联动
+
+
+![avatar](area_picker.png)
+
+
+![avatar](effect.gif)
---
+++ lib/widgets/model/area_model.dart
@@ -0,0 +1,24 @@
+
+class AreaModel {
+  int id;
+  String name;
+  List<AreaModel> childs;
+
+  static AreaModel fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+    AreaModel invoiceAreaBean = AreaModel();
+    invoiceAreaBean.id = map['id'];
+    invoiceAreaBean.name = map['name'];
+    invoiceAreaBean.childs = List()..addAll(
+        (map['childs'] as List ?? []).map((o) => AreaModel.fromMap(o))
+    );
+    return invoiceAreaBean;
+  }
+
+  Map toJson() => {
+    "id": id,
+    "name": name,
+    "childs": childs,
+  };
+}
+
